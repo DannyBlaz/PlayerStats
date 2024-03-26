d1018aca84b94115c0beb16cd88ada3d8f72f64d
Daniel <dannyblaze469@gmail.com>
2024-03-25 19:28:23 -0700

    Initial Commit

---
+++ PlayerStats/PlayerStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerStatsApp.swift
+//  PlayerStats
+//
+//  Created by Daniel Ezekiel on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayerStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
