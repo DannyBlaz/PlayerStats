610266f846e6025b5850c27a53954df0ef2d838b
Daniel <dannyblaze469@gmail.com>
2024-03-27 19:56:25 -0700

    Creating Playes Model

--- PlayerStats/ContentView.swift
+++ PlayerStats/ContentView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationLink {
+            Text("players")
+            Text("players")
+        } label: {
+            Image(systemName: "textformat")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
---
+++ PlayerStats/Player.swift
@@ -0,0 +1,10 @@
+//
+//  Player.swift
+//  PlayerStats
+//
+//  Created by Daniel Ezekiel on 3/27/24.
+//
+
+import Foundation
+
+
